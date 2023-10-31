ad1cd0011d2297e6cadc291a1dc21572d47e1e35
Stritos <121346177+Stritos@users.noreply.github.com>
2023-10-31 13:57:07 +0200

    Create ft_strstr.c

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuntean <amuntean@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 12:08:58 by amuntean          #+#    #+#             */
+/*   Updated: 2023/09/05 18:13:15 by amuntean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (to_find[0] == '\0')
+	{
+		return (str);
+	}
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && to_find[j] != '\0')
+		{
+			j++;
+		}
+		if (to_find[j] == '\0')
+		{
+			return (&str[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
+/*int	main(void)
+{
+	char	string[] = "to be found";
+	char	find[] = "a";
+	printf("String: %s\n", string);
+	printf("To find: %s\n", find);
+	printf("Result: %s\n", ft_strstr(string, find));
+	return (0);
+}*/
