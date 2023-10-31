db4091247245a53bfcc0633280b4c0f098b1df1f
Stritos <121346177+Stritos@users.noreply.github.com>
2023-10-31 13:49:33 +0200

    Create ft_sort_int_tab.c

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuntean <amuntean@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 20:06:39 by amuntean          #+#    #+#             */
+/*   Updated: 2023/08/23 14:29:31 by amuntean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	j = 0;
+	while (i < size -1)
+	{
+		j = i +1;
+		while (j < size)
+		{
+			if (tab[j] < tab[i])
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
