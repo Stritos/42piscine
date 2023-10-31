35ef98633e2866f8a55ea6bfdf5f1a7baa8dc33a
Stritos <121346177+Stritos@users.noreply.github.com>
2023-10-31 13:55:41 +0200

    Create ft_strcat.c

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuntean <amuntean@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 15:37:19 by amuntean          #+#    #+#             */
+/*   Updated: 2023/08/31 19:40:56 by amuntean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*int	main(void)
+{
+	char source[15] = "World";
+	char destination[100] = "Hello ";
+	printf("Destination: %s\n", destination);
+	printf("Source: %s\n", source);
+	printf("Destination + Source: %s\n", ft_strcat(destination, source));
+	return (0);
+}
+*/
