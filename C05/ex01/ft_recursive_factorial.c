2d3395af954e779f2d15179b2a23d1ba0716486c
Stritos <121346177+Stritos@users.noreply.github.com>
2023-10-31 14:11:29 +0200

    Create ft_recursive_factorial.c

---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuntean <amuntean@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 20:24:33 by amuntean          #+#    #+#             */
+/*   Updated: 2023/09/07 12:00:06 by amuntean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb == 0)
+	{
+		return (1);
+	}
+	if (nb < 0)
+	{
+		return (0);
+	}
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+/*int	main(void)
+{
+	printf("%d\n", ft_recursive_factorial(5));
+	return (0);
+}*/
