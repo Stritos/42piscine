42cc2cf981c7b29e1e304bf12df8b5a13b38a71f
Stritos <121346177+Stritos@users.noreply.github.com>
2023-10-31 13:49:11 +0200

    Create ft_rev_int_tab.c

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amuntean <amuntean@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 16:39:58 by amuntean          #+#    #+#             */
+/*   Updated: 2023/08/23 12:45:02 by amuntean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	start;
+	int	end;
+
+	start = 0;
+	end = size -1;
+	while (start < end)
+	{
+		temp = tab[start];
+		tab[start] = tab[end];
+		tab[end] = temp;
+		start++;
+		end--;
+	}
+}
